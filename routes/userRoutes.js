65be49f82c437039ea5f8cf3d362f167e4a9a17e
itsm22051@gmail.com <himalpandey2002@gmail.com>
2025-07-08 02:27:49 +0545

    Updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {registration, getAllUsers, getUserById, updateUser, deleteUser} = require('../controllers/userControllers');
+const { login}= require('../controllers/authController');
+const authenticateToken = require('../middlewares/authMiddleware');
 
 
+router.post("/login", login); // public
 router.post('/register', registration );            // http://localhost:5000/users/register
-router.get('/getallusers', getAllUsers);            // http://localhost:5000/users/getallusers
-router.get('/getuserbyid/:id', getUserById);        // http://localhost:5000/users/getuserbyid/id
-router.put('/updateuser/:id', updateUser);          // http://localhost:5000/users/updateuser/id
-router.delete('/deleteuser/:id', deleteUser);       // http://localhost:5000/users/deleteuser/id
+
+router.get('/getallusers', authenticateToken, getAllUsers);            // http://localhost:5000/users/getallusers
+router.get('/getuserbyid/:id', authenticateToken, getUserById);        // http://localhost:5000/users/getuserbyid/id
+router.put('/updateuser/:id',authenticateToken, updateUser);          // http://localhost:5000/users/updateuser/id
+router.delete('/deleteuser/:id',authenticateToken, deleteUser);       // http://localhost:5000/users/deleteuser/id
 
 
 module.exports = router;
